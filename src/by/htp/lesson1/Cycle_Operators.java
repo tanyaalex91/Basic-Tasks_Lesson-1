1f4c238ae28774522440485611a79849207191f7
tanyaalex91 <36223017+tanyaalex91@users.noreply.github.com>
2018-02-18 16:02:36 +0300

    Add files via upload

---
+++ src/by/htp/lesson1/Cycle_Operators.java
@@ -0,0 +1,62 @@
+package by.htp.lesson1;
+
+public class Cycle_Operators {
+
+	public static void main(String[] args) {
+
+		// //1.1 ������� �� ����� ��� ������ ����� �� 1 �� 100 (��������� ���� for)
+		for (int x = 2; x <= 100; x = x + 2) {
+			System.out.println(x);
+		}
+
+		System.out.println("===============================================");
+		//
+		// //1.2. ������� �� ����� ��� �������� ����� �� 1 �� 15 (�� ��������� ���� for)
+		int z = 1;
+		while (z <= 13) {
+
+			z = z + 2;
+			System.out.println(z);
+		}
+		System.out.println("========================================================");
+
+		// 1.4 ��������� ����� ���� �� 20 �� 200 � ��������� ������� �� ����� (���������
+		// ���� for)
+
+		int sum = 0;
+
+		for (int y = 20; y <= 200; y++) {
+			sum = sum + y;
+		}
+		System.out.println(sum);
+
+		System.out.println("===================================================");
+
+		//// 1.5 ������ ������ �� ������ �� 1 �� 12 � ������� �������� ������,
+		// ��������������� ������� ����� (��������� ���� � �������� if else)
+
+		for (int month = 1; month <= 12; month++) {
+
+			if (month == 1) {
+				System.out.println("January");
+			} else {
+				System.out.println("February");
+				System.out.println("March");
+				System.out.println("April");
+				System.out.println("May");
+				System.out.println("June");
+				System.out.println("July");
+				System.out.println("August");
+				System.out.println("September");
+				System.out.println("October");
+				System.out.println("November");
+				System.out.println("December");
+
+				break;
+			}
+
+		}
+
+	}
+
+}
